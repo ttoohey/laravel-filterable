c3d1c41e5e2f46b6165dd3304abbc7f05e7ca4d5
Tim Toohey <ttoohey@users.noreply.github.com>
2017-06-24 11:50:10 +1000

    add IN rule to integer type
    to be more useful when models use ints for keys

--- src/Type/IntegerType.php
+++ src/Type/IntegerType.php
@@ -11,6 +11,7 @@ class IntegerType implements FilterableType
     static function default () {
         return [
           Filterable::EQ,
+          Filterable::IN,
           Filterable::MIN,
           Filterable::MAX,
           Filterable::LT,
